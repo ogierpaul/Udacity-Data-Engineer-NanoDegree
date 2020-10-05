9ebb81f6bc4bec899e9c9ca7752cea8b30dd04ed
ogierpaul <ogier.paul@gmail.com>
2020-10-05 23:52:49 +0200

    updated queries

--- p3_datawarehouse_aws_redshift/src/sql_queries.py
+++ p3_datawarehouse_aws_redshift/src/sql_queries.py
@@ -106,7 +106,7 @@ create_table_queries = [staging_events_table_create, staging_songs_table_create,
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop,
                       song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [
-    #staging_events_copy,
+    staging_events_copy,
     staging_songs_copy
     ]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
