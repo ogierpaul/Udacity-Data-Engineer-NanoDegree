4848dd4296d3c69521606c7dc26cbc56332ef752
ogierpaul <ogier.paul@gmail.com>
2020-11-06 10:31:51 +0100

    fixed local spark session with as much options I could get

--- p4_datalake_spark/p4src/localsparksession.py
+++ p4_datalake_spark/p4src/localsparksession.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
 import os
 
-
 def create_local_session(config):
     """
     Launch a spark session configured with AWS credentials
@@ -32,30 +31,4 @@ def create_local_session(config):
     spark._jsc.hadoopConfiguration().set("fs.s3a.fast.upload.active.blocks", "4")
     spark._jsc.hadoopConfiguration().set("fs.s3a.committer.name", "magic")
     spark._jsc.hadoopConfiguration().set("fs.s3a.committer.staging.conflict-mode", "append")
-    return spark
-
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-import os
-
-def create_spark_session(config):
-    """
-    Generate a spark session
-    Args:
-        config (config): config file [AWS]:(KEY, SECRET)
-    Returns:
-        SparkSession
-    """
-    #os.environ['PYSPARK_PYTHON'] = "/usr/local/anaconda3/envs/dend/bin/python3"
-    # os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.hadoop:hadoop-aws:2.7.3 pyspark-shell"
-    # Set the AWS credentials
-    os.environ['AWS_ACCESS_KEY_ID']=config.get('AWS','KEY')
-    os.environ['AWS_SECRET_ACCESS_KEY']=config.get('AWS','SECRET')
-    spark = SparkSession \
-        .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.3") \
-        .getOrCreate()
-    spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.awsAccessKeyId", config.get("AWS", "KEY"))
-    spark._jsc.hadoopConfiguration().set("fs.s3a.awsSecretAccessKey", config.get("AWS", "SECRET"))
     return spark
